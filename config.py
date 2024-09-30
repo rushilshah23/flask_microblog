943142642a298b6e0017b32948fd7d6445ba9baa
Rushil Shah <61428940+rushilshah23@users.noreply.github.com>
2024-10-01 00:29:28 +0530

    Chapter 9

--- config.py
+++ config.py
@@ -13,3 +13,5 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['rushilshah88@gmail.com']
+
+    POSTS_PER_PAGE = 25
